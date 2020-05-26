dcb04aa05a8a34a2c863e7d7cfe9cebe6c5d7553
AtuheireElizabeth <atuheireelizabeth94@gmail.com>
2020-05-26 17:06:11 +0300

    final-copy

--- login.js
+++ login.js
@@ -1,15 +1,23 @@
 const url ="http://localhost:8080/formDb"
 // create user
-// const createUser =(user) =>{
-//     const userData =JSON.stringify(user);
-//     fetch(url),{
-//         method: "POST",
-//         body: userData,
-//         headers:{
-//             ("Accept:application/json",
-//             "Content-Type: application/json"
-//              ) }
-//     }
+const createUser = (user) => {
+    const userData = JSON.stringify(user);
+    fetch(url, {
+        method: "POST",
+        body: userData,
+        headers:{
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      (data);
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+};
 
 
 
@@ -25,15 +33,37 @@ const userId = document.getElementById("userId");
 logInBtn.addEventListener("click", (e) => {
     e.preventDefault();
     const userIdValue = userId.value;
+    const userIdLength = userIdValue.length
     const passwordValue = password.value;
     const passwordLength = passwordValue.length;
 
-    if (passwordLength < 5 ) {
-        alert("The password must have atleast 5 characters!");
+
+    if (userIdLength < 1 ) {
+        alert("Please Enter Your UserId!");
     }
+
+    // else {
+    //    if (passwordLength < 5 ) {
+    //     window.location.replace("./dash/dash_recruiter.html");
+    //     }
+
+
+
+
+
+
+
+    if (passwordLength < 1 ) {
+        alert("Please Enter Your Password!");
+    }
+
+    else {
+       if (passwordLength < 5 ) {
+        window.location.replace("./dash/dash_recruiter.html");
+        }
     else {
         // alert("logged in successfully"); 
         window.location.replace("./dash/dash.html");
     } 
 
-});
+}});
